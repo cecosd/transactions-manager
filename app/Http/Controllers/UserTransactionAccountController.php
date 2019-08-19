2f6901e01f95340c1114ff3a4e4f42b46d335674
Tsvetan <tsvetan.y.petrov@gmail.com>
2019-08-19 16:36:18 +0300

    controllers creation

---
+++ app/Http/Controllers/UserTransactionAccountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserTransactionAccount;
+use Illuminate\Http\Request;
+
+class UserTransactionAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserTransactionAccount  $userTransactionAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserTransactionAccount $userTransactionAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UserTransactionAccount  $userTransactionAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserTransactionAccount $userTransactionAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserTransactionAccount  $userTransactionAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserTransactionAccount $userTransactionAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserTransactionAccount  $userTransactionAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserTransactionAccount $userTransactionAccount)
+    {
+        //
+    }
+}
