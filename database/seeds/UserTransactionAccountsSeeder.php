ae90c72ed7a3533d37151916a0083c637e61b112
Tsvetan <tsvetan.y.petrov@gmail.com>
2019-08-20 14:45:52 +0300

    seeding amount check

--- database/seeds/UserTransactionAccountsSeeder.php
+++ database/seeds/UserTransactionAccountsSeeder.php
@@ -11,7 +11,7 @@ class UserTransactionAccountsSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 100)->create()->each(function ($user) {
+        factory(App\User::class, 10)->create()->each(function ($user) {
             
             factory(App\UserTransactionAccount::class)->create([
                 'user_id' => $user->id
